3f4fbb59e2dd314b67ee781cbb15e38d30472402
Aryan <aryaninmail@gmail.com>
2024-10-18 09:54:23 +0530

    update: aptitude action

--- src/lib/actions/aptitude.action.ts
+++ src/lib/actions/aptitude.action.ts
@@ -4,7 +4,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const getAptitudeDataFromGemini = async (
   subject: string,
-  numberOfQuestions: number = 15,
+  numberOfQuestions: number = 10,
 ) => {
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
