6b5f861a8c6b47176298fbf7f49d6ccc5b792cf3
Aryan <aryankinmail@gmail.com>
2025-01-14 12:26:00 +0530

    create: jobs UI

--- src/app/(home)/jobs/page.tsx
+++ src/app/(home)/jobs/page.tsx
@@ -4,40 +4,37 @@
 // import { getAllJobs } from "@/lib/actions/job.action";
 // import { MapPin } from "lucide-react";
 
+import { internships } from "@/constants";
+
 type Props = {};
 
 const Page = async (props: Props) => {
   // const res = await getAllJobs();
 
-  return (
-    <div className="flex h-full w-full items-center justify-between">
-      <p className="w-full text-center">Jobs | Coming soon...</p>
-    </div>
-  );
-
   // return (
-  //   <div className="custom-scrollbar scroll-m-20 space-y-2 overflow-scroll px-5 pb-20 pt-5 max-md:pt-20 md:w-1/2">
-  //     {res?.map((job: any, index: number) => (
-  //       <Card
-  //         key={index}
-  //         className="border-none px-4 py-2 shadow-sm hover:shadow-input dark:shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset]"
-  //       >
-  //         <div className="flex items-center justify-between gap-2">
-  //           <p className="font-space_grotesk">{job.companyName}</p>
-  //           <Badge>{job.jobType}</Badge>
-  //         </div>
-  //         <p className="py-1 text-sm text-muted-foreground">{job.experience}</p>
-
-  //         <div className="flex items-center gap-2 text-sm">
-  //           <MapPin size={18} />
-  //           <p className="text-sm font-light">{job.location}</p>
-  //           <Separator orientation="vertical" className="h-5" />
-  //           <p className="text-sm font-light">{job.category}</p>
-  //         </div>
-  //       </Card>
-  //     ))}
+  //   <div className="flex h-full w-full items-center justify-between">
+  //     <p className="w-full text-center">Jobs | Coming soon...</p>
   //   </div>
   // );
+
+  return (
+    <div className="custom-scrollbar scroll-m-20 space-y-2 overflow-scroll px-5 pb-20 pt-5 max-md:pt-20 md:w-1/2">
+      {internships.map((item, index) => (
+        <div
+          className="my-2 rounded-lg bg-zinc-300 p-2 dark:bg-zinc-800"
+          key={index}
+        >
+          <div className="flex items-center justify-between">
+            <p>{item.company}</p>
+            <p>{item.position}</p>
+          </div>
+          <p className="opacity-80">{item.location}</p>
+          <p className="opacity-50">{item.eligibility}</p>
+          <p>{item.skills}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Page;
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -19,6 +19,11 @@ export const SidebarLinks = [
         label: "Aptitude",
     },
     {
+        imgURL: <BriefcaseBusiness />,
+        route: "/jobs",
+        label: "Jobs",
+    },
+    {
         imgURL: <Smile />,
         route: "/profile",
         label: "Profile",
@@ -111,3 +116,160 @@ export const TrendingAptitudeItems = [
         link: "/aptitude/miscellaneous",
     },
 ];
+
+export const companies = [
+    "LinkedIn", "Twitter", "Microsoft", "Atlassian", "Walmart", "Oracle", "Oracle-Netsuite",
+    "SalesForce", "SAP Labs", "MindTickle", "VISA", "J.P.Morgan", "Intuit", "Qualcomm",
+    "Nvidia", "AMD", "Silicon Labs", "Teradata", "Tokopedia", "Mediatek", "Microchip",
+    "Bloomberg LP", "Mathworks", "DE Shaw", "Tekion Corp", "Yugabyte", "Grab", "Groupon",
+    "Delplix", "Indeed", "PaloAlto Networks", "Zoom", "Clear", "Adobe", "Samsung", "Oppo",
+    "Flipkart", "Myntra", "Meesho", "Ajio.com", "Nykaa", "AppDynamics", "Wells Fargo",
+    "Xilinx", "MotorQ", "Rubric", "Codenation", "Sprinklr", "Nutanix", "Cohesity",
+    "ShareChat", "Cure.fit", "Healthifyme", "SumoLogic", "Delhivery", "Qubole", "Trell",
+    "Autodesk", "Target", "Informatica", "Mastercard", "American Express",
+    "NK Securities Research", "Zeta", "Postman", "Cloudera", "Inmobi", "BNY Mellon",
+    "Bank of America", "NetApp", "Rupeek", "Intel", "Juniper Network", "Vmware",
+    "Duestche Bank", "Servicenow", "Ibibo Group", "Make my trip", "Fanclash", "Infoblox",
+    "Plivo", "Texas Instruments", "Directi", "Media.net", "Citicorp", "Riverbed Technologies",
+    "GE Healthcare", "Hp", "Dell", "Udaan", "Hotstar", "Phonepe", "Paytm", "Juspay",
+    "Razorpay", "Cashfree", "Gojek", "KLA", "Simpl"
+];
+
+export const internships = [
+    {
+        company: "Internkaksha IT Solutions",
+        position: "Frontend Developer Intern",
+        link: "Link",
+        location: "Remote",
+        eligibility: "Freshers can apply",
+        skills: "HTML, CSS, JavaScript, React.js"
+    },
+    {
+        company: "TwiLearn",
+        position: "Full Stack Developer Intern",
+        link: "Link",
+        location: "Remote",
+        eligibility: "Freshers can apply",
+        skills: "MERN Stack"
+    },
+    {
+        company: "Internkaksha IT Solutions",
+        position: "Web Development Intern",
+        link: "Link",
+        location: "Remote",
+        eligibility: "Freshers can apply",
+        skills: "HTML, CSS, JavaScript, React.js"
+    },
+    {
+        company: "Unified Mentor",
+        position: "Frontend Developer Intern",
+        link: "Link",
+        location: "Remote",
+        eligibility: "Freshers can apply",
+        skills: "HTML, CSS, JavaScript, React.js"
+    },
+    {
+        company: "ADP",
+        position: "Intern",
+        link: "Link",
+        location: "Chennai, IN",
+        eligibility: "Freshers can apply",
+        skills: "Strong understanding of fundamentals of CS"
+    },
+    {
+        company: "Abhyaz",
+        position: "Software Engineer Intern",
+        link: "Link",
+        location: "Chennai, IN",
+        eligibility: "Freshers can apply",
+        skills: "Strong understanding of fundamentals of CS"
+    },
+    {
+        company: "Coddle Technologies Pvt. Ltd.",
+        position: "Software Engineer Trainee",
+        link: "Link",
+        location: "Bengaluru, Karnataka, IN",
+        eligibility: "Freshers can apply",
+        skills: "Strong understanding of fundamentals of CS"
+    },
+    {
+        company: "Isourse",
+        position: "Backend Developer Intern",
+        link: "Link",
+        location: "Delhi, IN",
+        eligibility: "Freshers can apply",
+        skills: "HTML, CSS, JavaScript, Node.js, SQL"
+    },
+    {
+        company: "Unified Mentor",
+        position: "Web Development Intern",
+        link: "Link",
+        location: "Remote",
+        eligibility: "Freshers can apply",
+        skills: "MERN Stack"
+    },
+    {
+        company: "Bolt.Earth",
+        position: "Software Engineer Intern",
+        link: "Link",
+        location: "Bengaluru, Karnataka, IN",
+        eligibility: "Freshers can apply",
+        skills: "Strong understanding of fundamentals of CS and one of the coding languages"
+    },
+    {
+        company: "InnovatexIQ Solutions",
+        position: "SQL Developer",
+        link: "Link",
+        location: "Hyderabad, Telangana, India",
+        eligibility: "Freshers can apply",
+        skills: "Strong understanding of fundamentals of CS and Databases"
+    },
+    {
+        company: "Unified Mentor",
+        position: "Full Stack Web Developer",
+        link: "Link",
+        location: "Remote",
+        eligibility: "Freshers can apply",
+        skills: "MERN Stack"
+    },
+    {
+        company: "Applied Materials",
+        position: "Software Engineer Intern",
+        link: "Link",
+        location: "Bengaluru, Karnataka, IN",
+        eligibility: "Freshers can apply",
+        skills: "Strong understanding of fundamentals of CS and one of the coding languages"
+    },
+    {
+        company: "Bluelupin Technologies",
+        position: "Software Engineer Intern",
+        link: "Link",
+        location: "Noida, Uttar Pradesh, India",
+        eligibility: "Freshers can apply",
+        skills: "HTML, CSS, JavaScript"
+    },
+    {
+        company: "Refonte Learning",
+        position: "Full Stack Development training and Internship",
+        link: "Link",
+        location: "Remote",
+        eligibility: "Freshers can apply",
+        skills: "HTML, CSS, JavaScript, Node.js"
+    },
+    {
+        company: "Tower Research Capital",
+        position: "Intern",
+        link: "Link",
+        location: "Gurugram, Haryana, India",
+        eligibility: "Freshers can apply",
+        skills: "Strong understanding of fundamentals of CS and one of the coding languages"
+    },
+    {
+        company: "Zuddl",
+        position: "Engineer Intern - Fullstack",
+        link: "Link",
+        location: "Remote",
+        eligibility: "Freshers can apply",
+        skills: "Strong understanding of fundamentals"
+    }
+]
